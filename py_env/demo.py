e0678aaf5532f1a8b6d07c13a93bd3d87a877e06
coderodde <rodion.efremov@cs.helsinki.fi>
2016-11-29 16:02:09 +0200

    Added couple of dependencies.

--- py_env/demo.py
+++ py_env/demo.py
@@ -1,1 +1,11 @@
+from music21 import *
+
+#n = note.Note("D#3")
+#n.duration.type = 'half'
+#n.show('lily.pdf')
+
+littleMelody = converter.parse("tinynotation: 3/4 c4 d8 f g16 a g f#")
+littleMelody.show('lily')
+littleMelody.show('midi')
+littleMelody.write('midi', 'my_midi.mid')
 print("Hello, I am the demonstration Python script")
