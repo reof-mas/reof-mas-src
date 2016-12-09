5b65173fcccfb67f9afe8f3f45e7b562539d3a1a
Otto <otto.hantula@hotmail.com>
2016-12-09 19:36:58 +0000

    get_markov_chain now returns transition counts, agent calculates probabilities

--- py_env/src/melodic_chains.py
+++ py_env/src/melodic_chains.py
@@ -7,12 +7,12 @@ from creamas import Simulation
 def main():
     selected_order = 1
     directory_path = "../../melodies/classical/bach"
-    transition_probs = markov_chain.get_markov_chain(directory_path, order=selected_order)
+    transition_counts = markov_chain.get_markov_chain(directory_path, order=selected_order)
 
     env = MusicEnvironment.create(('localhost', 5555))
     env.log_folder = 'logs'
     for i in range(10):
-        agent = ComposerAgent(env, transition_probs)
+        agent = ComposerAgent(env, transition_counts)
 
     # Audience agents also?
 
