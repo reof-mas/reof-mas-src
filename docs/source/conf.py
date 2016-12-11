697112efcd7e72f597efd31496fe341e8dcd8967
Otto <otto.hantula@hotmail.com>
2016-12-11 22:46:02 +0000

    doc stuff

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -17,9 +17,9 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
+import os
+import sys
+sys.path.insert(0, os.path.abspath('../../py_env/src'))
 
 # -- General configuration ------------------------------------------------
 
@@ -33,6 +33,7 @@
 extensions = [
     'sphinx.ext.mathjax',
     'sphinx.ext.githubpages',
+    'sphinx.ext.autodoc',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
